9693d0e020f9aa237565e696792f164216fc5370
Perelmeshcka <yyariyyarikk@gmail.com>
2024-12-09 17:30:27 +0300

    clipboard history

--- hypr/src/start.sh
+++ hypr/src/start.sh
@@ -1,4 +1,9 @@
 # hyprpm reload -n
+
+wl-paste --type text --watch cliphist store &
+wl-paste --type image --watch cliphist store &
+
 swww-daemon &
 sh ~/.config/hypr/src/set-wallpaper.sh ~/Images/virus.png
+
 brightnessctl set 25%
